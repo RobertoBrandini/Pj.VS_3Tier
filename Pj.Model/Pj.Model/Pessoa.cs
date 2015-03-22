e37eadc15041735ae73994dd4860bd1bc6fcafc2
RobertoBrandini <dp.sistemas@gmail.com>
2015-03-22 01:36:34 -0300

    Classe Pessoa Modelo

---
+++ Pj.Model/Pj.Model/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pj_Model
+{
+    [System.ComponentModel.DataObject(true)]
+    public class Pessoa
+    {
+
+# pragma warning disable 1591
+
+        public System.Int32 ID_Pessoa { get; set; }
+        public System.String Nome { get; set; }
+
+#pragma warning restore 1591
+
+    }
+}
