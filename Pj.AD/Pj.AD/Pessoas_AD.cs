9bc9a5b9c98b715dae773f1697405a57dc571770
RobertoBrandini <dp.sistemas@gmail.com>
2015-03-22 01:33:27 -0300

    PJ Simples

---
+++ Pj.AD/Pj.AD/Pessoas_AD.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pj_AD
+{
+    public static class Pessoas_AD
+    {
+        public static DataTable ObterPessoas_AD()
+        {
+            DataTable dt = new DataTable();
+
+            DataColumn col1 = new DataColumn("ID_Pessoa");
+            DataColumn col2 = new DataColumn("Nome");
+
+            col1.DataType = System.Type.GetType("System.Int32");
+            col2.DataType = System.Type.GetType("System.String");
+
+            dt.Columns.Add(col1);
+            dt.Columns.Add(col2);
+
+            DataRow row = dt.NewRow();
+            row[col1] = 1;
+            row[col2] = "Joao";
+            dt.Rows.Add(row);
+
+            row = dt.NewRow();
+            row[col1] = 2;
+            row[col2] = "Maria";
+            dt.Rows.Add(row);
+
+            return dt;
+        }
+    }
+}
--- Pj.Web/Pj.Web/Default.aspx.cs
+++ Pj.Web/Pj.Web/Default.aspx.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace Pj.Web
+namespace Pj_Web
 {
     public partial class Default : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+        }
 
+        protected void btnLerPessoas_Click(object sender, EventArgs e)
+        {
+            DataTable dt = PJ_NG.Pessoas_NG.ObterPessoas_NG();
+            gvwPessoas.DataSource = dt;
+            gvwPessoas.DataBind();
         }
     }
 }
