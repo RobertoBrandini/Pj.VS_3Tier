01e685caaa996491111185f58ea082e507659cab
RobertoBrandini <dp.sistemas@gmail.com>
2016-04-22 23:54:53 -0300

    calculo

---
+++ Pj.NG/PJ.NG/Calculo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pj_NG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class Calculo
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="precoCompra"></param>
+        /// <param name="margemLucro"></param>
+        /// <returns></returns>
+        public static Decimal CalcularPrecoVenda(decimal precoCompra, decimal margemLucro)
+        {
+            return precoCompra * (1 + (margemLucro / 100));
+        }
+
+    }
+}
